0424b2fb75d2ac92203220bba8ae7f5a4e780232
jimmisD <57838154+jimmisD@users.noreply.github.com>
2020-06-03 16:54:10 +0300

    Script file to install libraries for back-end

---
+++ back-end/packages.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+
+pip3 install bcrypt
+
+pip3 install djangorestframework
+
+pip3 install django-excel
+
+pip3 install django
+
+pip3 install django-apikey
+
+pip3 install djangorestframework
+
+pip3 install markdown
+
+pip3 install django-filter
+
+pip3 install djangorestframework-api-key
+
+pip3 install pyexcel-xls
+
+pip3 install django-secure
+
+pip3 install django-sslserver
+
+pip3 install django-cors-headers
+
+
